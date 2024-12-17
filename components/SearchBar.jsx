e932b075bd065162732f97cd5f15b0405ec7e001
Aasim <m.aasimsamar7@gmail.com>
2024-12-17 08:27:52 +0530

    remove module css

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,15 +1,37 @@
-import styles from "./SearchFilter.module";
+import styled from 'styled-components';
+
+const SearchDiv =  styled.div`
+  background-color: #3b3838;
+  padding: 0.5rem 1.5rem;
+  border-radius: 5px;
+  border: 0.2px solid #666666;
+
+ & .input {
+  background-color: #3333;
+  border: none;
+  font-size: 16px;
+  margin-left: 10px;
+  caret-color: #fff;
+  font-weight: 500;
+  color: #dadada;
+
+  &:focus-visible {
+    outline: none;
+    border: none;
+  }
+}
+  `;
 
 export function SearchBar({ setQuery }) {
   return (
-    <div className={`${styles["search-container"]} search_Container`}>
+    <SearchDiv className="search-container search_Container">
       <i className="fa-solid fa-magnifying-glass"></i>
       <input
         onChange={(e)=>setQuery(e.target.value.toLowerCase())}
         type="text"
-        className={styles.input}
+        className="input"
         placeholder="Search for a country..."
       />
-    </div>
+    </SearchDiv>
   );
 }
--- components/SelectMenu.jsx
+++ components/SelectMenu.jsx
@@ -1,14 +1,33 @@
-import styles from './SearchFilter.module'
+import styled from 'styled-components';
+
+const Select = styled.select`
+  background-color: #3b3838;
+  padding: 0.5rem 2rem;
+  border-radius: 5px;
+  border: 0.2px solid #666666;
+  color: #dadada;
+  font-size: 16px;
+
+  &:focus-visible {
+    outline: none;
+    border: none;
+  }
+
+  & option {
+    color: #dadada;
+  }
+
+`;
 
 export default function SelectMenu({setQuery}) {
   return (
-    <select className={`${styles["filter-by-region"]} filter`} onChange={(e)=> setQuery(e.target.value.toLowerCase())}>
+    <Select className="filter-by-region filter" onChange={(e)=> setQuery(e.target.value.toLowerCase())}>
         <option hidden>Filter By Region</option>
         <option value="Africa">Africa</option>
         <option value="Americas">Americas</option>
         <option value="Asia">Asia</option>
         <option value="Europe">Europe</option>
         <option value="Oceania">Oceania</option>  
-    </select>
+    </Select>
   )
 }
